dab36422e137d904891e1e24049c1a26f943557f
Koza <kiskoza@gmail.com>
2021-12-17 21:27:21 +0100

    Cleanup websocket client code, communicate with automations

--- lib/home_assistant_engine/application.ex
+++ lib/home_assistant_engine/application.ex
@@ -7,14 +7,17 @@ defmodule HomeAssistantEngine.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
+    automations = [
+    ]
+
+    client = [
       # Starts a worker by calling: HomeAssistantEngine.Worker.start_link(arg)
-      {HomeAssistantEngine.Client, "ws://127.0.0.1:8123/api/websocket"}
+      {HomeAssistantEngine.Client, {"ws://127.0.0.1:8123/api/websocket", automations}}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: HomeAssistantEngine.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(client, opts)
   end
 end
--- lib/home_assistant_engine/client.ex
+++ lib/home_assistant_engine/client.ex
@@ -1,39 +1,56 @@
 defmodule HomeAssistantEngine.Client do
   use WebSockex
 
-  def start_link(url) do
-    WebSockex.start_link(url, __MODULE__, %{id: 0})
+  def start_link({url, automations}) do
+    # State is {id, pending, automations}
+    WebSockex.start_link(url, __MODULE__, {0, [], automations})
   end
 
-  def handle_connect(_conn, state) do
+  def handle_connect(_conn, {_, _, automations} = state) do
     IO.puts("connected")
+
+    automations
+    |> Enum.each(fn module -> GenServer.start_link(module, :ok, name: module) end)
+
     {:ok, state}
   end
 
-  def handle_disconnect(_connection_status_map, state) do
+  def handle_disconnect(_connection_status_map, {_, _, automations} = state) do
     IO.puts("disconnected")
+
+    automations
+    |> Enum.each(fn module -> module.stop end)
+
     {:ok, state}
   end
 
-  def handle_frame({type, msg}, %{id: id} = state) do
+  def handle_frame({type, msg}, {id, pending, automations} = state) do
     case Jason.decode(msg) do
-      {:ok, %{"type" => "auth_required"}} ->
-        {:ok, reply} =
-          Jason.encode(%{
-            type: "auth",
-            access_token: Application.get_env(:home_assistant_engine, __MODULE__)[:token]
-          })
+      {:ok, %{"type" => "auth_required"}} -> auth_request(state)
+      {:ok, %{"type" => "auth_ok"}} -> get_states_request(state)
+      {:ok, %{"type" => "result", "success" => true} = response} ->
+        {:ok, type, pending} = get_call_type(response["id"], pending, [])
 
-        {:reply, {:text, reply}, state}
+        case type do
+          :get_states ->
+            handle_get_states(response, state)
+            subscribe_events_request({id, pending, automations})
 
-      {:ok, %{"type" => "auth_ok"}} ->
-        {:ok, reply} =
-          Jason.encode(%{
-            id: id + 1,
-            type: "get_states"
-          })
+          :subscribe_events ->
+            IO.puts("Successfully subscribed to events")
+            {:ok, {id, pending, automations}}
 
-        {:reply, {:text, reply}, %{state | id: id + 1}}
+          _ ->
+            IO.puts("Unknown response #{inspect(type)} - #{inspect(msg)}")
+            {:ok, {id, pending, automations}}
+        end
+
+      {:ok, %{"type" => "event"} = response} ->
+        IO.puts("Got an event")
+        %{"old_state" => old_entity, "new_state" => new_entity} = response["event"]["data"]
+
+        automations |> Enum.each(fn module -> module.entity_changed(old_entity, new_entity) end)
+        {:ok, state}
 
       _ ->
         IO.puts("Received Message - Type: #{inspect(type)} -- Message: #{inspect(msg)}")
@@ -44,5 +61,55 @@ defmodule HomeAssistantEngine.Client do
   def handle_cast({:send, {type, msg} = frame}, state) do
     IO.puts("Sending #{type} frame with payload: #{msg}")
     {:reply, frame, state}
+  end
+
+  defp auth_request(state) do
+    {:ok, reply} =
+      Jason.encode(%{
+        type: "auth",
+        access_token: Application.get_env(:home_assistant_engine, __MODULE__)[:token]
+      })
+
+    {:reply, {:text, reply}, state}
+  end
+
+  defp get_states_request({id, pending, automations}) do
+    id = id + 1
+    IO.puts("Request current states")
+    {:ok, reply} =
+      Jason.encode(%{
+        id: id,
+        type: "get_states"
+      })
+
+    {:reply, {:text, reply}, {id, [{id, :get_states} | pending], automations}}
+  end
+
+  defp handle_get_states(%{"result" => results}, {_, _, automations}) do
+    IO.puts("Got states")
+    results
+    |> Enum.each(fn result ->
+      automations |> Enum.each(fn module -> module.set_entity(result) end)
+    end)
+  end
+
+  defp subscribe_events_request({id, pending, automations}) do
+    id = id + 1
+    {:ok, reply} = Jason.encode(%{
+      id: id,
+      type: "subscribe_events"
+    })
+
+    {:reply, {:text, reply}, {id, [{id, :subscribe_events} | pending], automations}}
+  end
+
+  defp get_call_type(_, [], _) do
+    {:error, :not_in_the_list}
+  end
+  defp get_call_type(id, [{id, type} | tail], remaining) do
+    {:ok, type, remaining ++ tail}
+  end
+  defp get_call_type(id, [head | tail], remaining) do
+    get_call_type(id, tail, [head | remaining])
   end
 end
