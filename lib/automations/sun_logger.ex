c07dceb4dfd804738dcce39ab7f3411bcd97bc82
Koza <kiskoza@gmail.com>
2022-01-04 17:10:45 +0100

    Remove dev notes from SunLogger

--- lib/automations/sun_logger.ex
+++ lib/automations/sun_logger.ex
@@ -1,6 +1,3 @@
-# Test with this data:
-# %{"attributes" => %{"azimuth" => 242.46, "elevation" => -6.13, "friendly_name" => "Sun", "next_dawn" => "2021-12-11T05:43:13.851580+00:00", "next_dusk" => "2021-12-10T15:30:53.574281+00:00", "next_midnight" => "2021-12-10T22:37:11+00:00", "next_noon" => "2021-12-11T10:36:57+00:00", "next_rising" => "2021-12-11T06:19:08.587083+00:00", "next_setting" => "2021-12-11T14:55:02.197506+00:00", "rising" => false}, "context" => %{"id" => "a6f0fbf8b4907f1cb643ea7572989424", "parent_id" => nil, "user_id" => nil}, "entity_id" => "sun.sun", "last_changed" => "2021-12-10T15:21:56.609852+00:00", "last_updated" => "2021-12-10T15:29:40.828520+00:00", "state" => "below_horizon"}
-
 defmodule Automations.SunLogger do
   use GenServer
 
