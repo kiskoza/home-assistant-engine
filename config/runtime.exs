64d04b2e49f90b42c32eaf23d6161aa0ad584674
Koza <kiskoza@gmail.com>
2022-02-13 11:06:22 +0100

    Remove env variable handling from the engine, it should be handled by the dependant projects

--- config/runtime.exs
+++
@@ -1,6 +0,0 @@
-import Config
-import Dotenvy
-
-source([".env", ".env.#{config_env()}", ".env.#{config_env()}.local"])
-
-config :home_assistant_engine, HomeAssistantEngine.Client, token: env!("TOKEN", :string!)
